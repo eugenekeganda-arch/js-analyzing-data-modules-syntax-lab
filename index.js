f5352fe6a7504295ff798ece4b40a32b6333d2b4
Eugene <eugenekeganda@icloud.com>
2025-12-15 14:28:35 +0300

    users

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+//: Require datejs 
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
 
+  // Step 3: Loop through args
+  for (let i = 0; i < args.length; i++) {
+    const userArray = args[i];
+
+    // Step 4: Merge arrays using spread operator
+    combinedObject.users.push(...userArray);
+  }
+
+  // Step 5: Add merge date
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy');
+
+  // Step 7: Return object
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
